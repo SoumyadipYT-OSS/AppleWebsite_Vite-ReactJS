826c4496a0f7d84dbae81cbbc4939f1d0259eceb
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-06-01 21:27:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/SoumyadipYT-OSS/AppleWebsite_Vite-ReactJS/localhost:5170",
   
 
   build: {
