0e4eaa00bb960426f85c4ec0f9d3a2bfad720355
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-06-01 21:50:35 +0530

    updated gitlab

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/AppleWebsite_Vite-ReactJS/',
 
   build: {
     chunkSizeWarningLimit: 2000,
